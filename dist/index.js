225c88a1ca32a621057ba6e53038ce0e2c6637bb
ranajahanzaib <iam@ranajahanzaib.com>
2021-10-16 20:24:45 +0500

    Update

--- dist/index.js
+++ dist/index.js
@@ -3,13 +3,13 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-Object.defineProperty(exports, "Box", {
+Object.defineProperty(exports, "SearchBox", {
   enumerable: true,
   get: function get() {
-    return _Box.default;
+    return _SearchBox.default;
   }
 });
 
-var _Box = _interopRequireDefault(require("./components/Box"));
+var _SearchBox = _interopRequireDefault(require("./components/SearchBox"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
